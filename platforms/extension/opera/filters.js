336fb9879130fc427104414185c3e70640d3b8d8
Dmitriy Seregin <105th@users.noreply.github.com>
2023-12-13 00:48:26 +0000

    skip ci. build from Wed Dec 13 00:48:26 UTC 2023

--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -1916,8 +1916,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.12.19",
-			"timeUpdated": "2023-12-12T22:32:50+0000",
+			"version": "2.3.12.20",
+			"timeUpdated": "2023-12-13T00:46:05+0000",
 			"languages": [],
 			"tags": [
 				10,
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -1323,8 +1323,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.12.19",
-			"timeUpdated": "2023-12-12T22:32:50+0000",
+			"version": "2.3.12.20",
+			"timeUpdated": "2023-12-13T00:46:05+0000",
 			"languages": []
 		},
 		{
