104542b470c043063e358a895c6b6f66ccec8959
Dmitriy Seregin <d.seregin@adguard.com>
2023-12-14 20:20:14 +0300

    rework scripts, improved readme

--- scripts/repository/update.sh
+++ scripts/repository/update.sh
@@ -2,6 +2,8 @@ set -x -e
 
 # Push updated filter lists to the repo (without patches).
 git status
-git add .
+git add report.txt
+git add filters
+git add platforms
 git diff-index --quiet HEAD || git commit -m "skip ci. build from $(date)"
 git push origin master
