cc8a277e9f18d18a0c2ee0dbee0d41150d8cc19c
Filters Builder <filters@adguard.com>
2023-12-20 02:38:17 +0000

    skip ci. build from Wed Dec 20 02:38:17 UTC 2023

--- platforms/extension/edge/filters.js
+++ platforms/extension/edge/filters.js
@@ -1916,8 +1916,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/edge/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.13.19",
-			"timeUpdated": "2023-12-20T01:44:37+0000",
+			"version": "2.3.13.20",
+			"timeUpdated": "2023-12-20T02:34:42+0000",
 			"languages": [],
 			"tags": [
 				10,
