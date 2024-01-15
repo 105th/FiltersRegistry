91be88b33dc933031a93b9717c5220c955059d02
Dmitriy Seregin <d.seregin@adguard.com>
2024-01-16 02:46:46 +0300

    update scripts

--- scripts/auto_build.sh
+++ scripts/auto_build.sh
@@ -38,19 +38,20 @@ echo "Selected mode: $MODE"
 
 # Depending on the mode, execute different commands
 if [[ "$MODE" == "all" ]]; then
-    # Build all filters
-    yarn build
-    # Set the time live of patches to '4 hours'
-    yarn build:patches --time=4 --resolution=h
+    # Build all filters except ours to keep 1 hour update cycle of patches
+    # for our filters.
+    yarn build --skip=$ADGUARD_FILTERS
+    # Set the time live of patches to '4 hours' in seconds
+    yarn build:patches --time=14400 --resolution=s --skip=$ADGUARD_FILTERS
 elif [[ "$MODE" == "adguard" ]]; then
     # Build specific AdGuard filters based on the filter IDs
     yarn build --include=$ADGUARD_FILTERS
-    # Set the time live of patches to '60 minutes'
-    yarn build:patches --time=60 --resolution=m
+    # Set the time live of patches to '60 minutes' in seconds
+    yarn build:patches --time=3600 --resolution=s --include=$ADGUARD_FILTERS
 fi
 
 # Validate platforms and locales
 yarn validate
 
 # Update built platforms and filters in the repository
-yarn push
+# yarn push
--- scripts/build/patches.js
+++ scripts/build/patches.js
@@ -11,10 +11,12 @@ const {
 const { findFiles } = require('../utils/find_files');
 
 /**
- * Parse command-cli parameters -t|--time and -r|--resolution
+ * Parse command-cli parameters -t|--time, -r|--resolution, -i|--include and -s|--skip
  */
 let time = 60;
 let resolution = 'm';
+let includedFilterIDs = [];
+let excludedFilterIDs = [];
 
 const args = process.argv.slice(2); // Get command line arguments
 args.forEach((val) => {
@@ -25,32 +27,56 @@ args.forEach((val) => {
     if (val.startsWith('-r=') || val.startsWith('--resolution=')) {
         resolution = val.slice(val.indexOf('=') + 1);
     }
+
+    if (val.startsWith('-i=') || val.startsWith('--include=')) {
+        const value = val.slice(val.indexOf('=') + 1);
+
+        includedFilterIDs = value
+            .split(',')
+            .map((x) => Number.parseInt(x, 10));
+    }
+
+    if (val.startsWith('-s=') || val.startsWith('--skip=')) {
+        const value = val.slice(val.indexOf('=') + 1);
+
+        excludedFilterIDs = value
+            .split(',')
+            .map((x) => Number.parseInt(x, 10));
+    }
 });
 
 /**
- * Copies old patch files from one directory to another.
+ * Moves the latest patch file from the old patches directory to a new directory.
  *
- * @param oldFilterDir The directory containing old patch files.
- * @param newFilterDir The directory where old patch files will be copied.
+ * @param oldPatchesDir The directory containing old patch files.
+ *
+ * @throws {Error} Throws an error if no patch files are found in the specified directory.
+ *
+ * @returns A promise that resolves when the patch file is successfully moved.
  */
-const copyOldPatches = async (oldFilterDir, newFilterDir) => {
-    const oldPatches = await findFiles(
-        oldFilterDir,
-        (file) => file.endsWith(PATCH_EXTENSION)
-    );
-
-    for (let i = 0; i < oldPatches.length; i += 1) {
-        const oldPatch = oldPatches[i];
-        const relativePath = path.relative(oldFilterDir, oldPatch);
-        const newPath = path.join(newFilterDir, relativePath);
-
-        try {
-            await fs.promises.mkdir(path.dirname(newPath), { recursive: true });
-            await fs.promises.copyFile(oldPatch, newPath);
-        } catch (error) {
-            console.error(`Error copying old patch ${oldPatch} to ${newPath}: ${error}`);
-        }
+const moveCreatedPatch = async (oldPatchesDir) => {
+    // It means, that we don't have any old patches for this filter.
+    if (!fs.existsSync(oldPatchesDir)) {
+        return;
     }
+
+    const files = await fs.promises.readdir(oldPatchesDir);
+
+    const patches = files
+        .filter((f) => f.endsWith(PATCH_EXTENSION))
+        .sort((a, b) => b.localeCompare(a));
+
+    if (patches.length === 0) {
+        throw new Error(`Not found patches in folder: "${oldPatchesDir}".`);
+    }
+
+    const lastPatch = patches[0];
+
+    const fullPath = path.join(oldPatchesDir, lastPatch);
+    const pathToMove = fullPath.replace(FOLDER_WITH_OLD_FILTERS, FOLDER_WITH_NEW_FILTERS);
+
+    await fs.promises.copyFile(fullPath, pathToMove);
+    console.log(`Moved new patch from "${fullPath}" to "${pathToMove}".`);
 };
 
 /**
@@ -60,7 +86,35 @@ const main = async () => {
     // Find all new filter files
     const newFilterFiles = await findFiles(
         FOLDER_WITH_NEW_FILTERS,
-        (file) => file.includes('filters/') && file.endsWith('.txt')
+        (file) => {
+            const fileInFiltersFolder = file.includes('filters/');
+            const fileHasTxtExtension = file.endsWith('.txt');
+
+            const filename = path.basename(file);
+
+            if (!/\d+(_optimized|_without_easylist)?\.txt/.test(filename)) {
+                console.log(`Skipped generating patch for: ${file}`);
+
+                return false;
+            }
+
+            const filterId = Number.parseInt(filename, 10);
+
+            const fileNotExcluded = excludedFilterIDs.length > 0
+                ? !excludedFilterIDs.includes(filterId)
+                : true;
+            const fileIncluded = includedFilterIDs.length > 0
+                ? includedFilterIDs.includes(filterId)
+                : true;
+
+            const res = fileInFiltersFolder && fileHasTxtExtension && fileNotExcluded && fileIncluded;
+
+            if (!res) {
+                console.log(`Skipped generating patch for: ${file}`);
+            }
+
+            return res;
+        }
     );
 
     for (let i = 0; i < newFilterFiles.length; i += 1) {
@@ -81,12 +135,14 @@ const main = async () => {
             name,
             time,
             resolution,
-            verbose: true,
+            verbose: false,
         });
-    }
 
-    // Copy old patches
-    await copyOldPatches(FOLDER_WITH_OLD_FILTERS, FOLDER_WITH_NEW_FILTERS);
+        // Patch to old filter recorded to temp/platforms folder, because old
+        // filters located inside this folder and path to patch is relative
+        // to filter.
+        await moveCreatedPatch(path.join(path.dirname(path.dirname(oldFilterPath)), 'patches', name));
+    }
 
     // Clear temporary copied platforms
     await fs.promises.rm(FOLDER_WITH_OLD_FILTERS, { recursive: true });
