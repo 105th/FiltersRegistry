3232bdfebf3e0d36a6a7f392d7a464858ce4a3b8
Filters Builder <filters@adguard.com>
2024-02-11 20:35:21 +0000

    skip ci. build from Sun Feb 11 20:35:21 UTC 2024

--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.20.9",
-			"timeUpdated": "2024-02-11T19:32:31+0000",
+			"version": "2.3.20.10",
+			"timeUpdated": "2024-02-11T20:33:09+0000",
 			"languages": []
 		},
 		{
