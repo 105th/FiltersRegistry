9524fa18c6818fb0f75d4d83bcb1aafff71134da
Filters Builder <filters@adguard.com>
2024-02-24 11:35:04 +0000

    skip ci. build from Sat Feb 24 11:35:04 UTC 2024

--- platforms/ios/filters.js
+++ platforms/ios/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/ios/filters/2_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.3.22.12",
-			"timeUpdated": "2024-02-24T10:33:24+0000",
+			"version": "2.3.22.13",
+			"timeUpdated": "2024-02-24T11:32:43+0000",
 			"languages": [],
 			"tags": [
 				10,
@@ -457,8 +457,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/ios/filters/7_optimized.txt",
 			"trustLevel": "full",
-			"version": "2.0.48.63",
-			"timeUpdated": "2024-02-24T10:33:41+0000",
+			"version": "2.0.48.64",
+			"timeUpdated": "2024-02-24T11:32:54+0000",
 			"languages": [
 				"ja"
 			],
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.22.12",
-			"timeUpdated": "2024-02-24T10:33:24+0000",
+			"version": "2.3.22.13",
+			"timeUpdated": "2024-02-24T11:32:43+0000",
 			"languages": []
 		},
 		{
@@ -128,8 +128,8 @@
 			"displayNumber": 1,
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/7.txt",
-			"version": "2.0.48.63",
-			"timeUpdated": "2024-02-24T10:33:41+0000",
+			"version": "2.0.48.64",
+			"timeUpdated": "2024-02-24T11:32:54+0000",
 			"languages": [
 				"ja"
 			]
