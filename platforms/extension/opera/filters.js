b92d42f512c187f746621a29c7259de2f255b3cf
Filters Builder <filters@adguard.com>
2024-02-28 07:35:40 +0000

    skip ci. build from Wed Feb 28 07:35:40 UTC 2024

--- platforms/extension/opera/filters.js
+++ platforms/extension/opera/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.22.65",
-			"timeUpdated": "2024-02-28T06:32:53+0000",
+			"version": "2.3.22.66",
+			"timeUpdated": "2024-02-28T07:33:03+0000",
 			"languages": [],
 			"tags": [
 				10,
@@ -588,8 +588,8 @@
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/14.txt",
 			"trustLevel": "full",
-			"version": "2.1.87.98",
-			"timeUpdated": "2024-02-27T22:32:20+0000",
+			"version": "2.1.87.99",
+			"timeUpdated": "2024-02-28T07:32:30+0000",
 			"languages": [],
 			"tags": [
 				5,
@@ -683,8 +683,8 @@
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/opera/filters/18.txt",
 			"trustLevel": "full",
-			"version": "2.0.33.99",
-			"timeUpdated": "2024-02-27T19:32:41+0000",
+			"version": "2.0.34.0",
+			"timeUpdated": "2024-02-28T07:32:36+0000",
 			"languages": [],
 			"tags": [
 				5,
--- platforms/mac/filters.js
+++ platforms/mac/filters.js
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.22.65",
-			"timeUpdated": "2024-02-28T06:32:53+0000",
+			"version": "2.3.22.66",
+			"timeUpdated": "2024-02-28T07:33:03+0000",
 			"languages": []
 		},
 		{
@@ -228,8 +228,8 @@
 			"displayNumber": 1,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/14.txt",
-			"version": "2.1.87.98",
-			"timeUpdated": "2024-02-27T22:32:20+0000",
+			"version": "2.1.87.99",
+			"timeUpdated": "2024-02-28T07:32:30+0000",
 			"languages": []
 		},
 		{
@@ -287,8 +287,8 @@
 			"displayNumber": 2,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/18.txt",
-			"version": "2.0.33.99",
-			"timeUpdated": "2024-02-27T19:32:41+0000",
+			"version": "2.0.34.0",
+			"timeUpdated": "2024-02-28T07:32:36+0000",
 			"languages": []
 		},
 		{
