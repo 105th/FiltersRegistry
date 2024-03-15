e383235e3dd882ecc44a70e4934f60489f8afbaa
Filters Builder <filters@adguard.com>
2024-03-15 00:49:18 +0000

    skip ci. build from Fri Mar 15 00:49:17 UTC 2024

--- platforms/extension/chromium/filters.js
+++ platforms/extension/chromium/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.25.7",
-			"timeUpdated": "2024-03-14T22:32:38+0000",
+			"version": "2.3.25.8",
+			"timeUpdated": "2024-03-15T00:46:47+0000",
 			"languages": [],
 			"tags": [
 				10,
@@ -397,8 +397,8 @@
 			"groupId": 3,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium/filters/4.txt",
 			"trustLevel": "full",
-			"version": "2.0.98.21",
-			"timeUpdated": "2024-03-14T19:33:11+0000",
+			"version": "2.0.98.22",
+			"timeUpdated": "2024-03-15T00:46:53+0000",
 			"languages": [],
 			"tags": [
 				3,
@@ -588,8 +588,8 @@
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium/filters/14.txt",
 			"trustLevel": "full",
-			"version": "2.1.89.51",
-			"timeUpdated": "2024-03-14T20:32:31+0000",
+			"version": "2.1.89.52",
+			"timeUpdated": "2024-03-15T00:46:19+0000",
 			"languages": [],
 			"tags": [
 				5,
@@ -607,8 +607,8 @@
 			"groupId": 6,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium/filters/15.txt",
 			"trustLevel": "full",
-			"version": "2.0.62.35",
-			"timeUpdated": "2024-03-14T18:32:19+0000",
+			"version": "2.0.62.36",
+			"timeUpdated": "2024-03-15T00:46:20+0000",
 			"languages": [],
 			"tags": [
 				45,
@@ -703,8 +703,8 @@
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium/filters/19.txt",
 			"trustLevel": "full",
-			"version": "2.0.32.64",
-			"timeUpdated": "2024-03-14T19:32:41+0000",
+			"version": "2.0.32.65",
+			"timeUpdated": "2024-03-15T00:46:27+0000",
 			"languages": [],
 			"tags": [
 				5,
@@ -1542,8 +1542,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/chromium/filters/224.txt",
 			"trustLevel": "full",
-			"version": "2.1.11.22",
-			"timeUpdated": "2024-03-14T23:32:47+0000",
+			"version": "2.1.11.23",
+			"timeUpdated": "2024-03-15T00:46:36+0000",
 			"languages": [
 				"zh"
 			],
--- platforms/mac/filters.json
+++ platforms/mac/filters.json
@@ -61,8 +61,8 @@
 			"displayNumber": 1,
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/2.txt",
-			"version": "2.3.25.7",
-			"timeUpdated": "2024-03-14T22:32:38+0000",
+			"version": "2.3.25.8",
+			"timeUpdated": "2024-03-15T00:46:47+0000",
 			"languages": []
 		},
 		{
@@ -87,8 +87,8 @@
 			"displayNumber": 1,
 			"groupId": 3,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/4.txt",
-			"version": "2.0.98.21",
-			"timeUpdated": "2024-03-14T19:33:11+0000",
+			"version": "2.0.98.22",
+			"timeUpdated": "2024-03-15T00:46:53+0000",
 			"languages": []
 		},
 		{
@@ -228,8 +228,8 @@
 			"displayNumber": 1,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/14.txt",
-			"version": "2.1.89.51",
-			"timeUpdated": "2024-03-14T20:32:31+0000",
+			"version": "2.1.89.52",
+			"timeUpdated": "2024-03-15T00:46:19+0000",
 			"languages": []
 		},
 		{
@@ -241,8 +241,8 @@
 			"displayNumber": 3,
 			"groupId": 6,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/15.txt",
-			"version": "2.0.62.35",
-			"timeUpdated": "2024-03-14T18:32:19+0000",
+			"version": "2.0.62.36",
+			"timeUpdated": "2024-03-15T00:46:20+0000",
 			"languages": []
 		},
 		{
@@ -300,8 +300,8 @@
 			"displayNumber": 2,
 			"groupId": 4,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/19.txt",
-			"version": "2.0.32.64",
-			"timeUpdated": "2024-03-14T19:32:41+0000",
+			"version": "2.0.32.65",
+			"timeUpdated": "2024-03-15T00:46:27+0000",
 			"languages": []
 		},
 		{
@@ -1018,8 +1018,8 @@
 			"displayNumber": 1,
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/mac/filters/224.txt",
-			"version": "2.1.11.22",
-			"timeUpdated": "2024-03-14T23:32:47+0000",
+			"version": "2.1.11.23",
+			"timeUpdated": "2024-03-15T00:46:36+0000",
 			"languages": [
 				"zh"
 			]
