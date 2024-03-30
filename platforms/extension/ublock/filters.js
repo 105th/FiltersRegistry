e5d3688337337d788b14a69148e614a126decbda
Filters Builder <filters@adguard.com>
2024-03-30 01:47:17 +0000

    skip ci. build from Sat Mar 30 01:47:17 UTC 2024

--- platforms/extension/ublock/filters.js
+++ platforms/extension/ublock/filters.js
@@ -1542,8 +1542,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/extension/ublock/filters/224.txt",
 			"trustLevel": "full",
-			"version": "2.1.12.3",
-			"timeUpdated": "2024-03-30T00:46:23+0000",
+			"version": "2.1.12.4",
+			"timeUpdated": "2024-03-30T01:45:03+0000",
 			"languages": [
 				"zh"
 			],
