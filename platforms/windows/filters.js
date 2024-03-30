f22e1f673ce56aa9e646096190c0f407332e08f9
Filters Builder <filters@adguard.com>
2024-03-30 22:35:08 +0000

    skip ci. build from Sat Mar 30 22:35:08 UTC 2024

--- platforms/windows/filters.js
+++ platforms/windows/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.27.35",
-			"timeUpdated": "2024-03-30T18:32:41+0000",
+			"version": "2.3.27.36",
+			"timeUpdated": "2024-03-30T22:32:43+0000",
 			"languages": [],
 			"tags": [
 				10,
