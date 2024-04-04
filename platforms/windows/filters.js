010a698ff72541c6355769ae2a23098823e9545b
Filters Builder <filters@adguard.com>
2024-04-04 02:37:49 +0000

    skip ci. build from Thu Apr  4 02:37:49 UTC 2024

--- platforms/windows/filters.js
+++ platforms/windows/filters.js
@@ -357,8 +357,8 @@
 			"groupId": 1,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/2.txt",
 			"trustLevel": "full",
-			"version": "2.3.27.87",
-			"timeUpdated": "2024-04-04T01:46:40+0000",
+			"version": "2.3.27.88",
+			"timeUpdated": "2024-04-04T02:35:12+0000",
 			"languages": [],
 			"tags": [
 				10,
@@ -1542,8 +1542,8 @@
 			"groupId": 7,
 			"subscriptionUrl": "https://filters.adtidy.org/windows/filters/224.txt",
 			"trustLevel": "full",
-			"version": "2.1.12.36",
-			"timeUpdated": "2024-04-04T01:46:31+0000",
+			"version": "2.1.12.37",
+			"timeUpdated": "2024-04-04T02:34:57+0000",
 			"languages": [
 				"zh"
 			],
